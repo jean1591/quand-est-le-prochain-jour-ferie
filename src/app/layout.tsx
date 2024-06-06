58ab6458b98ec91a74181f92fc9ae1deaa63b9cd
Jean Robertou <j-rob@live.fr>
2024-06-06 08:05:27 +0200

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,39 @@ import "./globals.css";
 
 import { Footer } from "./component/footer";
 import { Inter } from "next/font/google";
+import { Metadata } from "next";
 import { NavBar } from "./component/navBar";
 import { classNames } from "@/utils";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const metaDescription = "Trouvez la liste des prochains jours fériés Français";
+
+export const metadata: Metadata = {
+  title: {
+    template: "Quand est le prochain jour férié ?",
+    default: "Quand est le prochain jour férié ?",
+  },
+  description: metaDescription,
+  metadataBase: new URL(
+    "https://quand-est-le-prochain-jour-ferie.jeanrobertou.com/"
+  ),
+  openGraph: {
+    title: "Quand est le prochain jour férié ?",
+    description: metaDescription,
+    url: "https://quand-est-le-prochain-jour-ferie.jeanrobertou.com/",
+    siteName: "Quand est le prochain jour férié ?",
+    images: [
+      {
+        url: "/hero-profile.jpeg",
+        width: 500,
+        height: 500,
+      },
+    ],
+    type: "website",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
