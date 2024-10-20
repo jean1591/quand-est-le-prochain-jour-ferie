1dc39b2cd99d09fe9c6e0ce7d06b2c1702412468
Jean Robertou <j-rob@live.fr>
2024-10-20 09:12:38 +0200

    chore: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
-import { MetadataRoute } from "next";
+import { MetadataRoute } from 'next'
 
-const baseUrl = "https://quand-est-le-prochain-jour-ferie.jeanrobertou.com";
-const changeFrequency = "monthly";
+const baseUrl = 'https://quand-est-le-prochain-jour-ferie.jeanrobertou.com'
+const changeFrequency = 'monthly'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
@@ -11,5 +11,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency,
       priority: 1,
     },
-  ];
+    {
+      url: `${baseUrl}/jours-feries-2025`,
+      lastModified: new Date(),
+      changeFrequency,
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/jours-feries-2026`,
+      lastModified: new Date(),
+      changeFrequency,
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/jours-feries-2027`,
+      lastModified: new Date(),
+      changeFrequency,
+      priority: 0.7,
+    },
+  ]
 }
