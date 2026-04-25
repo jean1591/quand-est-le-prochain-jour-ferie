4582459f93d6a7799fa7409634e462bef729d2cd
Jean Robertou <j-rob@live.fr>
2026-04-25 19:44:07 +0200

    feat: update footer

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -11,49 +11,60 @@ export const Footer = () => {
     <footer className="w-full border-t border-gray-200 bg-gray-50">
       <div className="mx-auto max-w-7xl px-8 py-12">
         <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
-          <div className="flex items-center gap-2 font-bold text-on-surface">
-            <p>Fait avec ❤️ par</p>
-            <Link href="https://jeanrobertou.com" className="text-primary hover:underline">
-              Jean Robertou
+          <div className="space-y-2">
+            <p className="text-on-surface flex items-center gap-2 font-bold">
+              Quand est le prochain jour férié
+            </p>
+            <div className="flex gap-1 text-sm text-gray-500">
+              <p>Fait avec ❤️ par</p>
+              <Link href="https://jeanrobertou.com" className="hover:underline">
+                Jean Robertou
+              </Link>
+            </div>
+          </div>
+
+          <div className="text-on-surface-variant flex items-center justify-center space-x-8">
+            <Link
+              href="/jours-feries-2025"
+              className="hover:text-primary hover:underline"
+            >
+              2025
+            </Link>
+            <Link
+              href="/jours-feries-2026"
+              className="hover:text-primary hover:underline"
+            >
+              2026
+            </Link>
+            <Link
+              href="/jours-feries-2027"
+              className="hover:text-primary hover:underline"
+            >
+              2027
             </Link>
           </div>
 
-          <div className="flex items-center gap-x-2 text-on-surface-variant">
+          <div className="text-on-surface-variant flex items-center gap-x-2">
             <Link
               href="https://jeanrobertou.com"
               target="_blank"
-              className="flex items-center justify-center hover:text-primary"
+              className="hover:text-primary flex items-center justify-center"
             >
               <PiIdentificationBadge className="h-8 w-8" />
             </Link>
             <Link
               href="https://github.com/jean1591"
               target="_blank"
-              className="flex items-center justify-center hover:text-primary"
+              className="hover:text-primary flex items-center justify-center"
             >
               <PiGithubLogo className="h-8 w-8" />
             </Link>
             <Link
               href="https://www.linkedin.com/in/robertoujean/"
               target="_blank"
-              className="flex items-center justify-center hover:text-primary"
+              className="hover:text-primary flex items-center justify-center"
             >
               <PiLinkedinLogo className="h-8 w-8" />
-            </Link>
-          </div>
-        </div>
-
-        <div className="mt-8 text-center">
-          <p className="mb-2 font-bold text-on-surface">Jours fériés en</p>
-          <div className="flex items-center justify-center space-x-8 text-on-surface-variant">
-            <Link href="/jours-feries-2025" className="hover:text-primary hover:underline">
-              2025
-            </Link>
-            <Link href="/jours-feries-2026" className="hover:text-primary hover:underline">
-              2026
-            </Link>
-            <Link href="/jours-feries-2027" className="hover:text-primary hover:underline">
-              2027
             </Link>
           </div>
         </div>
